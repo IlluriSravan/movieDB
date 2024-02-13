418df4c6fa12e213d52a483ad7b5589188ab8605
IlluriSravan <148531850+IlluriSravan@users.noreply.github.com>
2024-02-13 12:58:29 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import './index.css'
-import {Component} from 'react'
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
 import Loader from 'react-loader-spinner'
 import MovieItem from '../MovieItem'
 import Header from '../Header'
@@ -11,60 +12,64 @@ const apiStatusConstants = {
   inProgress: 'IN_PROGRESS',
 }
 
-class Home extends Component {
-  state = {
-    jobsList: [],
-    apiStatus: apiStatusConstants.initial,
-    page: 2,
+const Home = () => {
+  const [jobsList, setJobsList] = useState([])
+  const [apiStatus, setApiStatus] = useState(apiStatusConstants.initial)
+  const [page, setPage] = useState(1)
+  const [inputValue, setInput] = useState('')
+  const onChangee = e => {
+    setInput(e.target.value)
   }
 
-  componentDidMount() {
-    this.getMovies()
-  }
+  useEffect(() => {
+    const getMovies = async () => {
+      setApiStatus(apiStatusConstants.inProgress)
+      const apiToken = '064c9bb31091edd79eafe73ca134e5fa'
+      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiToken}&language=en-US&page=${page}`
+      const response = await fetch(url)
+      if (response.ok === true) {
+        const data = await response.json()
 
-  getMovies = async () => {
-    const {page} = this.state
-    this.setState({
-      apiStatus: apiStatusConstants.inProgress,
-    })
-    const apiToken = '064c9bb31091edd79eafe73ca134e5fa'
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiToken}&language=en-US&page=${page}`
-    const response = await fetch(url)
-    if (response.ok === true) {
-      const data = await response.json()
-      console.log(data)
-      const updatedData = data.results.map(each => ({
-        backdropPath: each.backdrop_path,
-        id: each.id,
-        originalTitle: each.original_title,
-        overview: each.overview,
-        posterPath: each.poster_path,
-        voteAverage: each.vote_average,
-      }))
-      console.log(updatedData)
-      this.setState({
-        jobsList: updatedData,
-        apiStatus: apiStatusConstants.success,
-      })
-    } else {
-      this.setState({
-        apiStatus: apiStatusConstants.failure,
-      })
+        const updatedData = data.results.map(each => ({
+          backdropPath: `https://image.tmdb.org/t/p/w500${each.backdrop_path}`,
+          id: each.id,
+          originalTitle: each.original_title,
+          overview: each.overview,
+          posterPath: each.poster_path,
+          voteAverage: each.vote_average,
+        }))
+
+        setJobsList(updatedData)
+        setApiStatus(apiStatusConstants.success)
+      } else {
+        setApiStatus(apiStatusConstants.failure)
+      }
+    }
+    getMovies()
+  }, [page])
+
+  const nextPage = () => {
+    console.log('page', page)
+    if (page < 500) {
+      setPage(prev => prev + 1)
     }
   }
 
-  successView = () => {
-    const {jobsList} = this.state
-    return (
-      <ul className="movies-container">
-        {jobsList.map(each => (
-          <MovieItem key={each.id} details={each} />
-        ))}
-      </ul>
-    )
+  const previousPage = () => {
+    if (page > 1) {
+      setPage(prev => prev - 1)
+    }
   }
 
-  failureView = () => (
+  const successView = () => (
+    <ul className="movies-container">
+      {jobsList.map(each => (
+        <MovieItem key={each.id} details={each} />
+      ))}
+    </ul>
+  )
+
+  const failureView = () => (
     <div className="jobs-error-view-container">
       <img
         src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
@@ -78,35 +83,58 @@ class Home extends Component {
     </div>
   )
 
-  inProgressView = () => (
+  const inProgressView = () => (
     <div className="loader-container" data-testid="loader">
-      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+      <Loader type="TailSpin" color="blue" height="50" width="50" />
     </div>
   )
 
-  renderViews = () => {
-    const {apiStatus} = this.state
-
+  const renderViews = () => {
     switch (apiStatus) {
       case apiStatusConstants.success:
-        return this.successView()
+        return successView()
       case apiStatusConstants.failure:
-        return this.failureView()
+        return failureView()
       case apiStatusConstants.inProgress:
-        return this.inProgressView()
+        return inProgressView()
       default:
         return null
     }
   }
 
-  render() {
-    return (
-      <>
-        <Header />
-        <div>{this.renderViews()}</div>
-      </>
-    )
-  }
+  return (
+    <>
+      <Header />
+      <button type="button" className="next-page" onClick={previousPage}>
+        Previous Page
+      </button>
+      <button type="button" className="next-page" onClick={nextPage}>
+        Next Page
+      </button>
+      <p>Page:{page}</p>
+      <div className="inp">
+        <input
+          type="search"
+          placeholder="Search"
+          onChange={onChangee}
+          value={inputValue}
+        />
+        <Link to={`/search/${inputValue}`}>
+          <button type="button" className="sear">
+            Search
+          </button>
+        </Link>
+      </div>
+
+      <div>{renderViews()}</div>
+    </>
+  )
 }
 
 export default Home
+
+// state = {
+//     jobsList: [],
+//     apiStatus: apiStatusConstants.initial,
+//     page: 2,
+//   }
