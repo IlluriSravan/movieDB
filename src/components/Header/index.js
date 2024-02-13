a89e1aa1bb6af9bddbc5cbf8a17c11addc3567ca
IlluriSravan <148531850+IlluriSravan@users.noreply.github.com>
2024-02-13 12:57:33 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,31 @@
 import './index.css'
 import {Link} from 'react-router-dom'
 import {useState} from 'react'
-import {IoMdSearch} from 'react-icons/io'
 
-const Header = () => {
-  const [inputValue, setInput] = useState('')
-  const onChangee = e => {
-    setInput(e.target.value)
-  }
-  return (
-    <>
-      <div className="header">
+const Header = () => (
+  <>
+    <div className="header">
+      <div className="header-heading">
         <h1 className="header-heading">movieDB</h1>
-        <div className="head-right">
-          <Link className="option-items" to="/">
-            <h1>Popular</h1>
-          </Link>
-          <Link className="option-items" to="/top-rated">
-            <h1>Top Rated</h1>
-          </Link>
-          <Link className="option-items" to="/upcoming">
-            <h1>Upcoming</h1>
-          </Link>
-
-          <div className="inp">
-            <input
-              type="search"
-              placeholder="Search"
-              onChange={onChangee}
-              value={inputValue}
-            />
-            <Link to={`/search/${inputValue}`}>
-              <IoMdSearch />
-            </Link>
-          </div>
-        </div>
       </div>
-    </>
-  )
-}
+
+      <div className="head-right">
+        <Link className="option-items" to="/">
+          <h1 className="cat">Popular</h1>
+        </Link>
+        <Link className="option-items" to="/top-rated">
+          <h1 className="cat">Top Rated</h1>
+        </Link>
+        <Link className="option-items" to="/upcoming">
+          <h1 className="cat">Upcoming</h1>
+        </Link>
+      </div>
+    </div>
+  </>
+)
+
 export default Header
+// <img
+//                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwIL8bNPb4TxfgyTAKaXpNBuxLzztq7ZPuKg&usqp=CAU"
+//                 className="sear"
+//               />
